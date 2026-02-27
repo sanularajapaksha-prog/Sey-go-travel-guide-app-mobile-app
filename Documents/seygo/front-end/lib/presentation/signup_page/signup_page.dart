9490586c1e6a7504d8b1dcba39ab07878053c6ea
minodya2003 <minodyaj@gmail.com>
2026-02-28 01:25:52 +0530

    feat: route signup to register UI

---
+++ Documents/seygo/front-end/lib/presentation/signup_page/signup_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+import '../register_page/register_page.dart';
+
+class SignupPage extends StatelessWidget {
+  const SignupPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const RegisterPage();
+  }
+}
