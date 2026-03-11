4e6924ac90609dd7ffd54148e5aa18775925ab39
HYperx-7 <rizlansinnengb@gmail.com>
2026-03-11 16:00:58 +0530

    feat: setup favorites screen file and imports

---
+++ Documents/seygo/front-end/lib/presentation/favorite_screen/favorites_screen.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:sizer/sizer.dart';
+
+import '../../providers/favorites_provider.dart';
+import '../../widgets/custom_image_widget.dart';
