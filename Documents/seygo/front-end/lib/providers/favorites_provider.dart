7be15fc0c325baddf85479b930ae526bb8874b7b
HYperx-7 <rizlansinnengb@gmail.com>
2026-03-11 20:22:20 +0530

    feat: add FavoritesProvider for managing favorite places

---
+++ Documents/seygo/front-end/lib/providers/favorites_provider.dart
@@ -0,0 +1,56 @@
+import 'dart:collection';
+
+import 'package:flutter/foundation.dart';
+
+class FavoritePlace {
+  final int id;
+  final String name;
+  final String imageUrl;
+  final String location;
+  final String semanticLabel;
+
+  const FavoritePlace({
+    required this.id,
+    required this.name,
+    required this.imageUrl,
+    required this.location,
+    required this.semanticLabel,
+  });
+}
+
+class FavoritesProvider extends ChangeNotifier {
+  final List<FavoritePlace> _favorites = [];
+
+  UnmodifiableListView<FavoritePlace> get favorites =>
+      UnmodifiableListView(_favorites);
+
+  int get count => _favorites.length;
+
+  bool isFavorite(int id) {
+    return _favorites.any((place) => place.id == id);
+  }
+
+  void toggleFavorite(FavoritePlace place) {
+    final existingIndex =
+        _favorites.indexWhere((favorite) => favorite.id == place.id);
+    if (existingIndex >= 0) {
+      _favorites.removeAt(existingIndex);
+    } else {
+      _favorites.add(place);
+    }
+    notifyListeners();
+  }
+
+  void removeFavorite(int id) {
+    _favorites.removeWhere((place) => place.id == id);
+    notifyListeners();
+  }
+
+  void clearFavorites() {
+    if (_favorites.isEmpty) {
+      return;
+    }
+    _favorites.clear();
+    notifyListeners();
+  }
+}
