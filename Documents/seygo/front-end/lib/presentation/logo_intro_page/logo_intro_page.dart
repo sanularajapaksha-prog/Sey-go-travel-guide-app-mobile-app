7d43a10fe80f802b0ab030a93fa5663d70df6d71
minodya2003 <minodyaj@gmail.com>
2026-03-13 12:30:53 +0530

    Update splash and welcome screens

--- Documents/seygo/front-end/lib/presentation/logo_intro_page/logo_intro_page.dart
+++ Documents/seygo/front-end/lib/presentation/logo_intro_page/logo_intro_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 import '../../routes/app_routes.dart';
 
@@ -14,13 +15,15 @@ class _LogoIntroPageState extends State<LogoIntroPage>
   late final AnimationController _controller;
   late final Animation<double> _scale;
   late final Animation<double> _fade;
+  late final Animation<double> _slideY;
+  late final Animation<double> _tilt;
 
   @override
   void initState() {
     super.initState();
     _controller = AnimationController(
       vsync: this,
-      duration: const Duration(milliseconds: 900),
+      duration: const Duration(milliseconds: 1200),
     )..forward();
     _scale = CurvedAnimation(
       parent: _controller,
@@ -30,8 +33,20 @@ class _LogoIntroPageState extends State<LogoIntroPage>
       parent: _controller,
       curve: Curves.easeIn,
     );
+    _slideY = Tween<double>(begin: 32, end: 0).animate(
+      CurvedAnimation(
+        parent: _controller,
+        curve: Curves.easeOutCubic,
+      ),
+    );
+    _tilt = Tween<double>(begin: -0.05, end: 0).animate(
+      CurvedAnimation(
+        parent: _controller,
+        curve: Curves.easeOutBack,
+      ),
+    );
 
-    Future.delayed(const Duration(milliseconds: 1600), () {
+    Future.delayed(const Duration(milliseconds: 2600), () {
       if (!mounted) return;
       Navigator.pushReplacementNamed(context, AppRoutes.introWelcome);
     });
@@ -49,9 +64,14 @@ class _LogoIntroPageState extends State<LogoIntroPage>
       body: Container(
         decoration: const BoxDecoration(
           gradient: LinearGradient(
-            colors: [Color(0xFF0E7AD1), Color(0xFF1CB5E0)],
-            begin: Alignment.topLeft,
-            end: Alignment.bottomRight,
+            colors: [
+              Color(0xFFD6D8DC),
+              Color(0xFF2F343A),
+              Color(0xFF0D0F13),
+            ],
+            stops: [0.0, 0.48, 1.0],
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
           ),
         ),
         child: SafeArea(
@@ -60,28 +80,30 @@ class _LogoIntroPageState extends State<LogoIntroPage>
               animation: _controller,
               builder: (_, child) => Opacity(
                 opacity: _fade.value,
-                child: Transform.scale(
-                  scale: 0.8 + (_scale.value * 0.25),
-                  child: child,
+                child: Transform.translate(
+                  offset: Offset(0, _slideY.value),
+                  child: Transform.rotate(
+                    angle: _tilt.value,
+                    child: Transform.scale(
+                      scale: 0.9 + (_scale.value * 0.18),
+                      child: child,
+                    ),
+                  ),
                 ),
               ),
-              child: Container(
-                padding: const EdgeInsets.all(24),
-                decoration: BoxDecoration(
-                  color: Colors.white.withOpacity(0.9),
-                  borderRadius: BorderRadius.circular(32),
-                  boxShadow: const [
-                    BoxShadow(
-                      color: Color(0x33000000),
-                      blurRadius: 18,
-                      offset: Offset(0, 10),
+              child: Text(
+                'SeyGo',
+                style: GoogleFonts.pacifico(
+                  fontSize: 68,
+                  color: Colors.white,
+                  letterSpacing: 0.4,
+                  shadows: const [
+                    Shadow(
+                      color: Color(0x55000000),
+                      blurRadius: 14,
+                      offset: Offset(0, 5),
                     ),
                   ],
-                ),
-                child: Image.asset(
-                  'assets/images/seygo_logo.png',
-                  width: 180,
-                  fit: BoxFit.contain,
                 ),
               ),
             ),
